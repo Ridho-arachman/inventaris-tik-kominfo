ae801eb28bce664891ffb2256d9f7bed7009c428
Ridho-arachman <ridho.arachman22@gmail.com>
2025-12-16 13:31:00 +0700

    chore: upgared next js, prisma, dan prisma client

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
   reactCompiler: true,
+  turbopack: {
+    root: path.resolve(__dirname), // ← pastikan root = direktori proyek ini
+  },
   images: {
     remotePatterns: [
       {
